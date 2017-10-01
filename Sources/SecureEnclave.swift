4ba1097f8adf220960f09661a2e19e9f12a475dc
Dan Federman <dfed@me.com>
2017-10-01 14:28:54 -0700

    Remove unintended blank comment line

--- Sources/SecureEnclave.swift
+++ Sources/SecureEnclave.swift
@@ -5,7 +5,6 @@
 //  Created by Dan Federman on 9/19/17.
 //  Copyright © 2017 Square, Inc.
 //
-//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
